2197ee0f469746043c02c05b7cebca722954245e
Shubham giri <shubhamgiri12345678yo@gmail.com>
2026-03-18 21:59:09 +0530

    Add initial script.js with commented code examples

---
+++ script.js
@@ -0,0 +1,98 @@
+// console.log("Hello World");
+// var num=10;
+// console.log(num);
+// console.log(typeof(num));
+// num="Hello World";
+// console.log(num);
+// console.log(typeof(num));
+// num=true;
+// console.log(num);
+// console.log(typeof(num));
+// num=null;
+// console.log(num);
+// console.log(typeof(num));
+// num;
+// console.log(num);
+// console.log(typeof(num));
+// const sym1= Symbol(4)
+// console.log(sym1);
+// const sym2 =Symbol(4);
+// console.log(sym2);
+// if(sym1===sym2){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+// var count
+// document.write("Starting Loop "+ "<br>");
+// for(count=0;count<10;count++) {
+//     document.write("Current count:"+count);
+//     document.write("<br/>");
+// }
+// document.write("Loop stopped!");
+// const person={
+//     fname:"Ajay",
+//     lname:"Singh",
+//     age:25,
+// };
+// for(let x in person){
+//     console.log("person details:"+x+":"+person[x]);
+// }
+// var age = 20;
+// if(age>18){
+//     document.write("<b>You are eligible to cast the vote.</b>");
+// }
+// else{
+//     document.write("<b>You are not eligible to cast the vote.</b>");
+// }   
+    //  var grade='C';
+    //  document.write("Entering switch block<br />");
+    //  switch(grade){
+    //     case 'A':
+    //         document.write("Good job<br />");
+    //         break;      
+    //     case 'B':
+    //         document.write("Pretty good<br />");
+    //         break;
+    //     case 'C':
+    //         document.write("Passed<br />");
+    //         break;
+    //     case 'D':   
+    //         document.write("Not so good<br />");
+    //         break;
+    //     case 'F':
+    //         document.write("Failed<br />");
+    //         break;
+    //     default:
+    //         document.write("Invalid grade!<br />");
+    //  }
+// function myFunction(){
+//     alert("Hello World");
+// }
+// function sayHello(name,age){
+//     document.write(name+" is "+age+" years old.");
+// }
+// var person={
+//     name:'John Mac',
+//     age:30,
+//     isMarried:true,
+//     child:['cheeku'],
+//     address:{
+//         street:'vinay nagar'
+//     }
+// };
+// console.log(person);
+// var person2= new Object();
+// person2.name='Krishna';
+// person2.age=30;
+// person2.address={};
+// person2.address.street='nagar road';
+// console.log(person2);
+// difference b/w object and array 
+// array is ordered collection of data and object is collection of key value pairs
+// array can be accessed by index and object can be accessed by key
+// array can contain any type of data and object can contain any type of data
+var myinfo = new Array(1,2,3,);
+var myinfo1 = Array("abc");
+console.log(myinfo,myinfo1);
